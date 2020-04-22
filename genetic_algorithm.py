7b24c449bf07a9158dbbc33093a37fe188ce8dfe
Bandy <pemisin.toyo@gmail.com>
2020-04-22 15:59:40 -0400

    Update genetic_algorithm.py

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -23,6 +23,18 @@ def create_population(size):
     return directions
 
 
+# def get_cardinal_direction(path):
+#     count = 0
+#
+#     for letter in path:
+#         if letter == "L":
+#             count = count - 1
+#         if letter == "R":
+#             count = count + 1
+#         if letter
+#
+
+
 def create_hp_model(filename):
     # H-Hydrophobic, F-Hydrophilic
     amino_dict = {"X": "H", "A": "H", "R": "F", "N": "F", "D": "F",
@@ -75,7 +87,7 @@ def create_hp_model(filename):
         # This contains the fit children
         fit_directions = []
         for direction in directions:
-            if not direction.__contains__("LLL") | direction.__contains__("RRR"):
+            if not direction.__contains__("LLLL") | direction.__contains__("RRRR"):
                 fit_directions.append(direction)
         # This happens if all children have bumps. In this case try making a new population until you have kids that
         # don't have bumps
@@ -85,10 +97,96 @@ def create_hp_model(filename):
         else:
             has_kids = True
 
-        # At this point fit_directions contains the directions that don't cause bumps.
-        # Use the directions to plot the Hydrophobic or Hydrophillic properties in a 2-D array.
+    # At this point fit_directions contains the directions that don't cause bumps.
+    # Use the directions to plot the Hydrophobic or Hydrophillic properties in a 2-D array.
+    # First get the cardinal direction based off of each direction.
+    count = 0
+    d = "W"
+    facing = []
+    fd = []
+    for direction in fit_directions:
+        for letter in direction:
+            # remove fd later
+            fd.append(letter)
 
-    print(fit_directions)
+            if letter == "L":
+                count = count - 1
+            elif letter == "R":
+                count = count + 1
+            else:
+                count = count + 0
+
+            if count % 4 == 0:
+                d = "E"
+            elif count % 4 == 1:
+                d = "S"
+            elif count % 4 == 2:
+                d = "W"
+            elif count % 4 == 3:
+                d = "N"
+            facing.append(d)
+
+        # At this point we have the correct cardinal directions based off of each direction we had to go in.
+        # Fill out a 2D matrix that is sequence_length by sequence_length; starting from the middle;
+        # in the lattice represent H(Hydrophobic) as 1 and F(Hydrophyllic) as 0
+        x = int(sequence_length / 2)
+        y = int(sequence_length / 2)
+        lattice = [[2] * sequence_length] * sequence_length
+        matrix = np.array(lattice)
+
+        # If the sequence starts with a Hydrophobic protein insert a "1" at the center of the matrix insert "0"
+        # otherwise
+        if h_f_list[0] == "H":
+            matrix[x][y] = 1
+        else:
+            matrix[x][y] = 0
+        for i in range(sequence_length):
+            if facing[i - 1] == "N":
+                if fd[i] == "L":
+                    y = y - 1
+                elif fd[i] == "R":
+                    y = y + 1
+                elif fd[i] == "F":
+                    x = x + 1
+
+            elif facing[i - 1] == "E":
+                if fd[i] == "L":
+                    x = x + 1
+                elif fd[i] == "R":
+                    x = x - 1
+                elif fd[i] == "F":
+                    y = y + 1
+
+            elif facing[i - 1] == "S":
+                if fd[i] == "L":
+                    y = y + 1
+                elif fd[i] == "R":
+                    y = y - 1
+                elif fd[i] == "F":
+                    x = x - 1
+
+            elif facing[i - 1] == "W":
+                if fd[i] == "L":
+                    x = x - 1
+                elif fd[i] == "R":
+                    x = x + 1
+                elif fd[i] == "F":
+                    y = y - 1
+
+            if h_f_list[i] == "H":
+                matrix[x][y] = 1
+            else:
+                matrix[x][y] = 0
+
+
+
+
+
+
+
+        print(fd)
+        print(matrix)
+    print(facing)
     return 0
 
 
